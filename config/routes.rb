5db182472ba198aeada68a9682a1c7bb2faad65e
yuimusic551-hub <yuimusic551@gmail.com>
2026-03-09 23:56:14 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
+  devise_for :users
   root to: "messages#index"
-devise_for :users
+  resources :users, only: [:edit, :update]
 end
-
