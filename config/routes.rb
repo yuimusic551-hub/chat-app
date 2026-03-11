8f16351270edae92807fbd10f6bf40cbfd1f1d96
yuimusic551-hub <yuimusic551@gmail.com>
2026-03-11 15:53:08 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  devise_scope :user do
-    authenticated :user do
-      root to: "messages#index", as: :authenticated_root
-    end
-
-    unauthenticated do
-      root to: "devise/sessions#new", as: :unauthenticated_root
-    end
-  end
-
+  root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
