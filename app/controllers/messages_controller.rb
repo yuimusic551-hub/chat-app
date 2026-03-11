295afb61f042720403d8c1ba3adf6c6def96dca5
yuimusic551-hub <yuimusic551@gmail.com>
2026-03-11 23:35:01 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,25 @@
 class MessagesController < ApplicationController
-  def index
-    @rooms = current_user.rooms
+ def index
+    @message = Message.new
+  @room = Room.find(params[:room_id])
+  @rooms = current_user.rooms
+  @messages = @room.messages.includes(:user)
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def message_params
+     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
 end
